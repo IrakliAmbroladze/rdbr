c7b51d1803eda5cdfa127707cb782d7bf0d2328c
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-03-17 13:33:15 +0400

    feat(tasks): add draft tasks-list component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Filters from "@/components/filters";
+import TasksList from "@/components/tasks-list";
 
 export default function Home() {
   return (
     <>
       <div>დავალებების გვერდი</div>
       <Filters />
-      <div>Tasks List</div>
+      <TasksList />
     </>
   );
 }
