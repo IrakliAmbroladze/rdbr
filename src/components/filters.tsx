216296ecdb51be04a2fcb4ab7cc6d0410cc20817
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-03-17 13:50:31 +0400

    feat(filters): add filter options

--- src/components/filters.tsx
+++ src/components/filters.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+const filterOptions = ["დეპარტამენტი", "პრიორიტეტი", "თანამშრომელი"];
+
 const Filters = () => {
-  return <div>Filters</div>;
+  return (
+    <ul className="flex gap-2.5 ">
+      {filterOptions.map((option, index) => (
+        <li key={index} className="cursor-pointer">
+          {option}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Filters;
