fe73f0a4759b0793cd010dda2dd35544c82b0048
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-03-17 13:31:03 +0400

    feat(filters): add draft filters component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Filters from "@/components/filters";
+
 export default function Home() {
   return (
     <>
       <div>დავალებების გვერდი</div>
+      <Filters />
+      <div>Tasks List</div>
     </>
   );
 }
