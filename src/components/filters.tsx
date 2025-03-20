a0501732bf32e0180709e79fa5549d653bdb2967
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-03-20 10:14:29 +0400

    feat(priorities): add ui filtering

--- src/components/filters.tsx
+++ src/components/filters.tsx
@@ -1,13 +1,60 @@
-import React from "react";
+"use client";
+import { Priority } from "@/types/priority";
+import { fetchPriorities } from "@/utils/fetch-priorities";
+import React, { useEffect, useState } from "react";
 
 const filterOptions = ["დეპარტამენტი", "პრიორიტეტი", "თანამშრომელი"];
 
 const Filters = () => {
+  const [openModalIndex, setOpenModalIndex] = useState<number | null>(null);
+  const [priorities, setPriorities] = useState<Priority[]>([]);
+
+  useEffect(() => {
+    const loadPriorities = async () => {
+      setPriorities(await fetchPriorities());
+    };
+
+    loadPriorities();
+  }, []);
+
   return (
-    <ul className="flex gap-2.5 ">
+    <ul className="flex gap-2.5">
       {filterOptions.map((option, index) => (
         <li key={index} className="cursor-pointer">
-          {option}
+          <button
+            onClick={() =>
+              setOpenModalIndex(openModalIndex === index ? null : index)
+            }
+          >
+            {option}
+          </button>
+          {openModalIndex === index && (
+            <div>
+              {openModalIndex === 1 && (
+                <form>
+                  <fieldset>
+                    <div className="flex flex-col">
+                      {priorities.map((priority) => (
+                        <label key={priority.id}>
+                          <input
+                            type="checkbox"
+                            id={priority.name}
+                            name="priorities"
+                            value={priority.id}
+                          />
+                          {priority.name}
+                        </label>
+                      ))}
+                    </div>
+
+                    <div>
+                      <button type="submit">არჩევა</button>
+                    </div>
+                  </fieldset>
+                </form>
+              )}
+            </div>
+          )}
         </li>
       ))}
     </ul>
